0b8026a1f2d9739ee75cee220d8348d821fc8644
roger zhao <general_zyj@163.com>
2017-12-16 12:49:17 +0800

    add config to script

--- cross_compile.sh
+++ cross_compile.sh
@@ -4,9 +4,8 @@ date
 echo "==================================================================================="
 echo "start execute $0"
 echo "make clean"
-# make distclean 
-# make clean 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- KSRC=../ KVER=4.1.13 
+make clean 
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- KSRC=../ KVER=4.4.32
 
 echo "end execute $0"
 echo "==================================================================================="
--- hostapd-0.8/hostapd/cross_compile.sh
+++ hostapd-0.8/hostapd/cross_compile.sh
@@ -6,6 +6,7 @@ echo "start execute $0"
 echo "make clean"
 # make distclean 
 # make clean 
+cp config_farring .config
 make ARCH=arm CC=arm-linux-gnueabihf-gcc clean all
 
 echo "end execute $0"
